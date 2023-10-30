8946f1a03fc9e2d9ef8c238df5df867747736e4a
gitobimma <obimma.igor@gmail.com>
2023-10-30 11:49:51 +0100

    bugfixes + fonts

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import "../footer/footer.css";
+import { Link } from "react-router-dom";
 // import linkedin from "../../assets/LinkedinOrange.svg";
 
 export const Footer = () => {
   return (
     <div>
       <div className="footer-container">
-        <img
-          className="linkedin"
-          src="./LinkedinOrange.svg"
-          alt="linkedin-icon"
-        />
+        <Link to="https://www.linkedin.com/in/igor-obimma-489a8527a/">
+          <img
+            className="linkedin"
+            src="./LinkedinOrange.svg"
+            alt="linkedin-icon"
+          />
+        </Link>
         <p className="copyright">© 2023 Obimma . All Rights Reserved. </p>
       </div>
     </div>
